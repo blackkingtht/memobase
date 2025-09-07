949b073555289b73918b54445a6de3d73ee45bf8
stan <459889714@qq.com>
2025-09-07 21:38:19 +0800

    fix utils

--- src/server/api/memobase_server/llms/utils.py
+++ src/server/api/memobase_server/llms/utils.py
@@ -1,12 +1,15 @@
-from openai import AsyncOpenAI
+import logging
+
+from openai import AsyncOpenAI, AsyncAzureOpenAI
 from volcenginesdkarkruntime import AsyncArk
 from ..env import CONFIG
+from ..env import LOG
 
 _global_openai_async_client = None
 _global_doubao_async_client = None
 
 
-def get_openai_async_client_instance() -> AsyncOpenAI:
+def get_openai_async_client_instance_bak() -> AsyncOpenAI:
     global _global_openai_async_client
     if _global_openai_async_client is None:
         _global_openai_async_client = AsyncOpenAI(
@@ -14,6 +17,18 @@ def get_openai_async_client_instance() -> AsyncOpenAI:
             api_key=CONFIG.llm_api_key,
             default_query=CONFIG.llm_openai_default_query,
             default_headers=CONFIG.llm_openai_default_header,
+        )
+    return _global_openai_async_client
+
+
+def get_openai_async_client_instance() -> AsyncAzureOpenAI:
+    global _global_openai_async_client
+    if _global_openai_async_client is None:
+        _global_openai_async_client = AsyncAzureOpenAI(
+            azure_endpoint=CONFIG.llm_base_url,
+            api_key=CONFIG.llm_api_key,
+            azure_deployment=CONFIG.best_llm_model,
+            api_version=CONFIG.llm_openai_default_query["api_version"]
         )
     return _global_openai_async_client
 
